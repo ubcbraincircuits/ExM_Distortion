a8e527e01b9cbecedaec81097a51bdf6835c4452
nick michelson <nick.michelson94@gmail.com>
2020-04-24 15:19:28 -0700

    added method for quantifying distortion after masking vector field

--- distortionEstimator.m
+++ distortionEstimator.m
@@ -1,148 +1,140 @@
-% %% Estimate distortion for ExM samples
-% % 
-% % Pipeline:
-% %   Align planes
-% %       Register expanded image to non-expanded image using scale, rotation, and translation
-% %       Find "best" transformation
-% %           Use transformation from best matching channel or take mean
-% %   Calculate expansion factor
-% %   Apply non-rigid registration to scaled image to find local distortions
-% %   
-% %   
-% %
-% % To do:
-% %   Align each image from Z-dimension and across channels and find the best
-% %   fitting frame
-% %
-% %   Compare B-Spline non-rigid registration method to current
-% %   
-% %   Quantify distortions
+%% Estimate distortion for ExM samples
 % 
-% clear, clc
-% 
-% %% Load data
-% 
-% dataPath = 'D:\UBC\Databinge\tutor\ExM_Distortion\';
-% before = loadTif([dataPath, 'BeforeMAPprocedure-Parv488-Syt2-568-vGAT-647-STACK.tif']);
-% after = loadTif([dataPath, 'AfterMAPprocedure-Parv488-Syt2-568-vGAT-647-STACK.tif']);
-% 
-% 
-% %% Separate channels
-% 
-% numChans = 4;
-% b = uint16(separateChannels(before, numChans));
-% a = uint16(separateChannels(after, numChans));
-% 
-% %% Find best matching slices for each channel
-% 
-% % note this can take over an hour
-% % tic
-% % [matchingSlices, corrMat] = matchSlices(b, a);
-% % toc
-% 
-% matchingSlices = [4, 2; 5, 5; 1, 12; 9, 11];
-% % 
-% % figure, 
-% % for C = 1:numChans    
-% %     subplot(4,1,C), imagesc(corrMat(:,:,C)), colormap jet;
-% %     ylabel('before'), xlabel('after'), title(['channel ', num2str(C)])
-% %     c = colorbar; c.Label.String = 'Correlation';
-% % end
-% 
-% %% Choose the best channel for further analysis
-% 
-% idx = evaluateMatches(b, a, matchingSlices);
-% 
-% fixed = b(:,:,matchingSlices(idx,1), idx);
-% moving = a(:,:,matchingSlices(idx,2), idx);
-% 
-% 
-% %% Visualize
-% excessPixels = size(moving)-size(fixed);
-% visFixed = padarray(fixed, excessPixels/2, 0, 'both');
-% figure, imagesc([visFixed moving])
-% 
-% 
-% % register
-% [optimizer, metric] = imregconfig('monomodal');
-% moving = imhistmatch(moving, fixed);
-% tform = imregtform(moving, fixed, 'similarity', optimizer, metric);
-% movingRegistered = imwarp(moving,tform,'OutputView',imref2d(size(fixed)));
-% 
-% figure, imshowpair(fixed, movingRegistered)
-% 
-% 
-% % Estimate expansion factor. This assumes expansion is equal in x and y.
-% % Check tform.T(1,1) and tform.T(2,2) for expansion in x and y respectively
-% expansionFactor = 1/sqrt(det(tform.T)); 
-% 
-% 
-% %% non-rigid registration
-% 
-% 
-% AFS = 1.0; % AccumulatedFieldSmoothing
-% % This parameter controls the amount of diffusion-like regularization.
-% % imregdemons applies the standard deviation of the Gaussian smoothing to
-% % regularize the accumulated field at each iteration. Larger values result
-% % in smoother output displacement fields. Smaller values result in more
-% % localized deformation in the output displacement field. 
-% % Values typically are in the range [0.5, 3.0].
-% 
-% 
-% [D,movingReg] = imregdemons(movingRegistered,fixed,[500 400 200], ...
-%     'AccumulatedFieldSmoothing', AFS);
-% 
-% figure, imshowpair(fixed, movingReg)
-% 
-% %% Visualize local distortions 
-% 
-% % resize for ease of visualization
-% % simpfixed = imresize(fixed, 1/13);
-% simpfixed = fixed;
-% simpD = imresize(D, 1/13);
-% % simpD = D;
-% 
-% 
-% [x,y] = meshgrid(0:13:size(simpfixed,1)-1, 0:13:size(simpfixed,2)-1);
-% u = simpD(:,:,1);
-% v = simpD(:,:,2);
-% 
-% figure, imshowpair(movingReg, movingRegistered)
-% hold on, quiver(x,y,-u,-v)
-% % axis([0, size(simpfixed,2)-1, 0, size(simpfixed,1)-1])
-% 
+% Pipeline:
+%   Align planes
+%       Register expanded image to non-expanded image using scale, rotation, and translation
+%       Find "best" transformation
+%           Use transformation from best matching channel or take mean
+%   Calculate expansion factor
+%   Apply non-rigid registration to scaled image to find local distortions
+%   Generate vector field describing distortions
+%   Quantify distortion using a pair-wise subtraction of all vectors
+%   
+%
+% To do:
+%
+%   Compare Elastix method to current
+%   Compare B-Spline non-rigid registration method to current
 
 
-%%
-% numBins = 10;
-% [N, edges, bins] = histcounts(pxDist, numBins);
+clear, clc
+
+%% Load data
+
+dataPath = 'D:\UBC\Databinge\tutor\ExM_Distortion\';
+before = loadTif([dataPath, 'BeforeMAPprocedure-Parv488-Syt2-568-vGAT-647-STACK.tif']);
+after = loadTif([dataPath, 'AfterMAPprocedure-Parv488-Syt2-568-vGAT-647-STACK.tif']);
+
+
+%% Separate channels
+
+numChans = 4;
+b = uint16(separateChannels(before, numChans));
+a = uint16(separateChannels(after, numChans));
+
+%% Find best matching slices for each channel
+
+% note this can take over an hour
+% tic
+% [matchingSlices, corrMat] = matchSlices(b, a);
+% toc
+
+matchingSlices = [4, 2; 5, 5; 1, 12; 9, 11];
 % 
-% clear tt
-% for i = 1:numBins
-%     idx = bins == i;
-%     tt(i) = mean(relError(idx));
-%     sem(i) = std(relError(idx));
-% 
+% figure, 
+% for C = 1:numChans    
+%     subplot(4,1,C), imagesc(corrMat(:,:,C)), colormap jet;
+%     ylabel('before'), xlabel('after'), title(['channel ', num2str(C)])
+%     c = colorbar; c.Label.String = 'Correlation';
 % end
-% 
-% figure, plot(edges(1:end-1),tt),
-% hold on, plot(edges(1:end-1), tt+sem)
+
+%% Choose the best channel for further analysis
+
+idx = evaluateMatches(b, a, matchingSlices);
+
+fixed = b(:,:,matchingSlices(idx,1), idx);
+moving = a(:,:,matchingSlices(idx,2), idx);
+
+
+%% Visualize
+excessPixels = size(moving)-size(fixed);
+visFixed = padarray(fixed, excessPixels/2, 0, 'both');
+figure, imagesc([visFixed moving])
+
+
+% register
+[optimizer, metric] = imregconfig('monomodal');
+moving = imhistmatch(moving, fixed);
+tform = imregtform(moving, fixed, 'similarity', optimizer, metric);
+movingRegistered = imwarp(moving,tform,'OutputView',imref2d(size(fixed)));
+
+figure, imshowpair(fixed, movingRegistered)
+
+
+% Estimate expansion factor. This assumes expansion is equal in x and y.
+% Check tform.T(1,1) and tform.T(2,2) for scale in x and y respectively
+expansionFactor = 1/sqrt(det(tform.T)); 
+
+
+%% non-rigid registration
+
+
+AFS = 1.0; % AccumulatedFieldSmoothing
+% This parameter controls the amount of diffusion-like regularization.
+% imregdemons applies the standard deviation of the Gaussian smoothing to
+% regularize the accumulated field at each iteration. Larger values result
+% in smoother output displacement fields. Smaller values result in more
+% localized deformation in the output displacement field. 
+% Values typically are in the range [0.5, 3.0].
+
+
+[D,movingReg] = imregdemons(movingRegistered,fixed,[500 400 200], ...
+    'AccumulatedFieldSmoothing', AFS);
+
+figure, imshowpair(fixed, movingReg)
+
+%% Visualize local distortions 
+
+% resize for ease of visualization
+% simpfixed = imresize(fixed, 1/13);
+simpfixed = fixed;
+simpD = imresize(D, 1/13);
+% simpD = D;
+
+
+[x,y] = meshgrid(0:size(D,1)-1, 0:size(D,2)-1);
+u = simpD(:,:,1);
+v = simpD(:,:,2);
+
+figure, imshowpair(movingReg, movingRegistered)
+hold on, quiver(x,y,-u,-v)
+% axis([0, size(simpfixed,2)-1, 0, size(simpfixed,1)-1])
+
 
 %%
-simpD = imresize(D, 1/13);
-tic
-[rr, dd] = quantifyDistortion(simpD);
-toc
+
+[err, pDist] = quantifyDistortions2(D, movingRegistered);
+%%
+numBins = 100;
+[N, edges, bins] = histcounts(pDist, numBins);
+
+clear tt labs sem
+for i = 1:numBins
+    idx = bins == i;
+    tt(i) = mean(err(idx));
+    sem(i) = std(err(idx));
 
 
-tic
-[relError2, pxDist] = quantifyDistortion2(simpD);
-toc
+end
+
+figure, plot(edges(2:end), tt,'k'),
+hold on, plot(edges(2:end), tt+sem, 'k--'), 
+plot(edges(2:end), tt-sem,'k--')
+xlabel('distance (px)')
+ylabel('error (px)')
 
 
-figure, plot(sort(relError))
-hold on
-plot(sort(rr))
+
 %% helper functions
 
 function I = loadTif(tifFile)
@@ -250,68 +242,117 @@ end
 
 
 
-function [relError, pxDist] = quantifyDistortion(D)
+function [relError, pDist] = quantifyDistortion(D)
 
-[H,W,Z] = size(D);
+[H, W, ~] = size(D);
+D2 = reshape(D, [H*W, 2]);
 
-D2 = reshape(D, [H*W, Z]);
-
+% preallocate
 relError = zeros( (H*W)^2, 1);
-pxDist = zeros( (H*W)^2, 1);
+pDist = zeros( (H*W)^2, 1);
 
 count = 1;
-
-% f = waitbar(0, 'Calculating error');
 for i = 1:size(D2,1)
-    for j = 1:size(D2,1)       
-        % calculate pixel to pixel distance
+    for j = 1:size(D2,1)              
+        % get pixel locations
         [y1, x1] = ind2sub([H W], i);
         [y2, x2] = ind2sub([H W], j);
-        pxDist(count) = sqrt( (x1 - x2)^2 + (y1 - y2)^2);        
         
-        % calculate relative error from vector field
+        % calculate pixel to pixel distance
+        pDist(count) = sqrt( (x1 - x2)^2 + (y1 - y2)^2);        
+        
+        % calculate difference between vectors at two points
         relError(count) = sqrt(...
             (D(y1,x1,1)-D(y2,x2,1))^2 + (D(y1,x1,2)-D(y2,x2,2))^2 );
         
         % update counter
         count = count+1;
     end
-
-%     waitbar(i/size(D2,1), f, 'Calculating error');
 end
-
-% close(f)
 end
 
 
 
-function [relError, pxDist] = quantifyDistortion2(D)
+function [err, pDist] = quantifyDistortion2(D, movingRegistered)
 
-[H,W,Z] = size(D);
-D2 = reshape(D, [H*W, Z]);
+% threshold image using Otsu's method
+level = graythresh(movingRegistered);
+mask = imbinarize(movingRegistered, level);
 
-numComparisons = (H*W)^2;
+% mask vector field and get x and y vectors in 2 arrays
+maskD = D .* mask;
+X = maskD(:,:,1);
+Y = maskD(:,:,2);
 
-relError = zeros(numComparisons,1);
-pxDist = zeros(numComparisons,1);
+% get indices for all mask vectors
+[row, col] = find(X~=0);
 
-Dtemp1 = D(:,:,1);
-Dtemp2 = D(:,:,2);
-parfor idx = 1:numComparisons
-    [i, j] = ind2sub(size(D2), idx);
+% preallocate
+N = numel(row);
+pDist = zeros(N^2, 1);
+err = zeros(N^2, 1);
+
+count = 1;
+
+for i = 1:N
+    % get pixel coordinates
+    yi = row(i);     xi = col(i);
+    for j = 1:N
+        % get pixel coordinates
+        yj = row(j);     xj = col(j);
+        
+        % calculate pixel to pixel distance
+        pDist(count) = sqrt( (xi-xj)^2 + (yi-yj)^2 );
+        
+        % calculate difference between vectors
+        err(count) = sqrt( (X(yi,xi)-X(yj,xj))^2 + (Y(yi,xi)-Y(yj,xj))^2 );
     
-    % get pixel locations
-    [y1, x1] = ind2sub([H W], j);
-    [y2, x2] = ind2sub([H W], i);
-    
-    % calculate distance
-    pxDist(idx) = sqrt( (x1 - x2)^2 + (y1 - y2)^2 );
-    
-    % calculate difference between vectors at two points
-    relError(idx) = sqrt(...
-        (D(y1,x1,1)-D(y2,x2,1))^2 + (D(y1,x1,2)-D(y2,x2,2))^2 ...
-        );
-
+        % update counter
+        count = count+1;
+    end
+end
 
 end
+
+
+
+
+function [err, pDist] = quantifyDistortion2(D, movingRegistered)
+
+% threshold image using Otsu's method
+level = graythresh(movingRegistered);
+mask = imbinarize(movingRegistered, level);
+
+% mask vector field and get x and y vectors in 2 arrays
+maskD = D .* mask;
+X = maskD(:,:,1);
+Y = maskD(:,:,2);
+
+% get indices for all mask vectors
+[row, col] = find(X~=0);
+
+% preallocate
+N = numel(row);
+pDist = zeros(N^2, 1);
+err = zeros(N^2, 1);
+
+count = 1;
+
+for i = 1:N   
+    for j = 1:N
+        % get pixel coordinates
+        yi = row(i);     xi = col(i);
+        yj = row(j);     xj = col(j);
+        
+        % calculate pixel to pixel distance
+        pDist(count) = sqrt( (xi-xj)^2 + (yi-yj)^2 );
+        
+        % calculate difference between vectors
+        err(count) = sqrt( (X(yi,xi)-X(yj,xj))^2 + (Y(yi,xi)-Y(yj,xj))^2 );
+    
+        % update counter
+        count = count+1;
+    end
+end
+
 end
